3ba9db96d2459df02dda3f014e2518e8e4278541
ariflateefshah <arif@ideas2it.com>
2019-09-09 16:29:26 +0530

    Feature: Declaring variables;

--- hello-world.ts
+++ hello-world.ts
@@ -1,3 +1,17 @@
-var test = () => 'test response';
+// Declaring Variables
 
-test();
+var rollnumber : number;
+
+rollnumber = 10;            //Correct
+rollnumber ='arif';         //In-correct
+rollnumber = true;          //In-correct
+rollnumber = {};            //In-correct
+
+// It will show error even while declaring variable
+
+
+var fname : string;
+
+fname = 'arif';               //Correct
+fname = 10;                   //In-correct
+ 
